7faa75887683185a2b0eaf5ed09d23d9ed8b33a7
nina_smirnova <kiep13@bk.ru>
2020-12-31 15:20:51 +0300

    feat: add ability to add validation

--- force-app/main/default/lwc/validationBuilderScreenBody/validationBuilderScreenBody.js
+++ force-app/main/default/lwc/validationBuilderScreenBody/validationBuilderScreenBody.js
@@ -1,9 +1,23 @@
-import { LightningElement, api, track } from "lwc";
+import { LightningElement, api, track, wire } from "lwc";
+import VALIDATION_OBJECT from "@salesforce/schema/Validation__c";
+import OPERATOR_FIELD from "@salesforce/schema/Validation__c.Operator__c";
+import { getPicklistValues } from "lightning/uiObjectInfoApi";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
 import createValidationList from "@salesforce/apex/ValidationController.createValidationList";
 
 export default class ValidationBuilderScreenBody extends LightningElement {
   @api questions;
   @api validations;
+
+  @wire(getObjectInfo, { objectApiName: VALIDATION_OBJECT })
+  validationObjectInfo;
+
+  @track secondQuestion;
+  @track firstQuestion;
+
+  operators;
+  @track displayedOperators;
+  @track selectedOperator;
 
   @track displayedValidations;
   @track validation;
@@ -14,12 +28,38 @@ export default class ValidationBuilderScreenBody extends LightningElement {
 
   connectedCallback() {
     if (this.questions) {
-      this.isHaveQuestions = this.questions.length > 0;
+      this.isHaveQuestions = this.questions.length > 2;
     } else {
       this.isHaveQuestions = false;
     }
+
+    if (this.isHaveQuestions) {
+      this.firstQuestion = this.questions[0].Position__c;
+      this.secondQuestion = this.questions[1].Position__c;
+    }
+
     this.displayedValidations = JSON.parse(JSON.stringify(this.validations));
     this.initValidations();
+  }
+
+  @wire(getPicklistValues, {
+    recordTypeId: "$validationObjectInfo.data.defaultRecordTypeId",
+    fieldApiName: OPERATOR_FIELD
+  })
+  initTypes({ error, data }) {
+    if (data) {
+      this.operators = data.values.map((item) => {
+        return {
+          label: item.label,
+          value: item.value
+        };
+      });
+      this.displayedOperators = [...this.operators];
+      this.selectedOperator = this.displayedOperators[0].value;
+    } else if (error) {
+      console.log(error);
+      this.error = true;
+    }
   }
 
   initValidations() {
@@ -40,9 +80,21 @@ export default class ValidationBuilderScreenBody extends LightningElement {
     return this.questions.map((question) => {
       return {
         label: question.Label__c,
-        value: question.Id
+        value: question.Position__c
       };
     });
+  }
+
+  setDisplayedOperators() {
+    let resolvedOperators = [...this.operators];
+    console.log(this.firstQuestion.Required__c);
+    if (this.firstQuestion.Required__c) {
+      resolvedOperators = resolvedOperators.filter((operator) => {
+        return !operator.label.toLowerCase().includes("null");
+      });
+    }
+    this.displayedOperators = [...resolvedOperators];
+    this.selectedOperator = this.displayedOperators[0].value;
   }
 
   sendValidationsChange() {
@@ -52,19 +104,42 @@ export default class ValidationBuilderScreenBody extends LightningElement {
     this.dispatchEvent(changeEvent);
   }
 
-  //----------unchecked---------
-
-  value = "inProgress";
-
-  get options() {
-    return [
-      { label: "New", value: "new" },
-      { label: "In Progress", value: "inProgress" },
-      { label: "Finished", value: "finished" }
-    ];
+  selectFirstQuestion(event) {
+    this.firstQuestion = this.questions.filter((question) => {
+      return question.Position__c === +event.detail.value;
+    })[0];
+    this.setDisplayedOperators();
   }
 
-  handleChange(event) {
-    this.value = event.detail.value;
+  selectSecondQuestion(event) {
+    this.secondQuestion = this.questions.filter((question) => {
+      return question.Position__c === +event.detail.value;
+    })[0];
+  }
+
+  addValidation() {
+    const input = this.template.querySelector(".input");
+
+    if (!this.firstQuestion || !this.secondQuestion || !input.validity.valid) {
+      return;
+    }
+
+    const validation = {
+      Related_Question__c: this.firstQuestion,
+      Dependent_Question__c: this.secondQuestion,
+      Operator__c: this.selectedOperator,
+      Value__c: input.value
+    };
+
+    this.displayedValidations.push(validation);
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.firstQuestion = this.questions[0].Position__c;
+    this.secondQuestion = this.questions[1].Position__c;
+    this.selectedOperator = this.displayedOperators[0].value;
+    const input = this.template.querySelector(".input");
+    input.value = "";
   }
 }
--- force-app/main/default/lwc/validationCard/validationCard.js
+++ force-app/main/default/lwc/validationCard/validationCard.js
@@ -1,8 +1,10 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 import DELETE_ICON from "@salesforce/resourceUrl/DeleteIcon";
 
 export default class ValidationCard extends LightningElement {
   deleteIcon = DELETE_ICON;
+
+  @api validation;
 
   deleteValidation() {}
 }
