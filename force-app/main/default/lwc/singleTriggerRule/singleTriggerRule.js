bfb434d8e06996e374414beaf16b2847e53f1b1b
Yulia <kisliuk-y@mail.ru>
2021-01-16 16:42:58 +0300

    refactor: change check for filled trigger rule fields

--- force-app/main/default/lwc/singleTriggerRule/singleTriggerRule.js
+++ force-app/main/default/lwc/singleTriggerRule/singleTriggerRule.js
@@ -246,7 +246,7 @@ export default class SingleTriggerRule extends LightningElement {
   isTriggerRuleClear(triggerRule) {
     let isClear = true;
     for(let field in triggerRule) { 
-      if(triggerRule[field] !== "") {
+      if(triggerRule[field] !== "" || !triggerRule[field]) {
         isClear = false;
         break;
       }
